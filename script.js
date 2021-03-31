4c339d9ff08570129489408a3c9ccc689bf8bc18
Thao Nguyen <79836025+thaonguyen1012@users.noreply.github.com>
2021-03-31 18:14:11 +0200

    create hamburger menu

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
