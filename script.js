27162ebb6dae8f7f2c502b8a3925b782317815cb
Thao Nguyen <79836025+thaonguyen1012@users.noreply.github.com>
2021-05-12 16:50:34 +0200

    delete hamburger menu

--- script.js
+++ script.js
@@ -11,12 +11,3 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
-
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
